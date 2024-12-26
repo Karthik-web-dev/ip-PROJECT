c51df559ac1ab1579dc4e56321619e597773100f
Karthik-web-dev <akshaykarthikakella2k20@gmail.com>
2024-12-26 13:09:53 +0530

    homepage completed and cart page html completed

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import random
 
 df = pd.read_csv('data.csv')
-cart = pd.DataFrame()
-print(df)
+cart = pd.DataFrame(columns=["name", "price", "ram", "storage", "battery", "quantity"])
 
 app = Flask(__name__)
 
 
 @app.route('/home')
 def home():
-    #selected = df['phone_model'].sample(n=6).tolist()
-    mobile_names = df['phone_model'].sample(n=6).tolist()
+    rand_ind = random.sample(range(len(df)), 3)
+    mobile_names = df.iloc[rand_ind][["phone_model", "price_usd", "ram", "storage", "battery"]].values.tolist()
+    print(mobile_names)
 
-    return render_template('home.html', mobile_names=mobile_names)
+    max_row = df.loc[df['price_usd'].idxmax()]
+    min_row = df.loc[df['price_usd'].idxmin()]
+    result = [[max_row['phone_model'], max_row['price_usd'], max_row['ram'], max_row['storage'], max_row['battery'], "most expensive", "warning"],
+              [min_row['phone_model'], min_row['price_usd'], min_row['ram'], min_row['storage'], min_row['battery'], "cheapest deal", "success"]]
+             
+
+    return render_template('home.html', rand_mobs=mobile_names, heir_mobs=result)
+
+
+
+cart = pd.DataFrame(columns=["name", "price", "ram", "storage", "battery", "quantity"])
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    item = {
+        "name": request.form.get("name"),
+        "price": request.form.get("price"),
+        "ram": int(request.form.get("ram")),
+        "storage": int(request.form.get("storage")),
+        "battery": int(request.form.get("battery")),
+        "quantity":1
+    }
+    # Append item to the cart DataFra
+    cart.loc[len(cart)] = item
+    return redirect(url_for('home'))
 
 @app.route('/cart')
-def cart():
-    
+def mycart():
+    print(cart)
     return render_template('cart.html')
 
 if __name__ =='__main__':  
